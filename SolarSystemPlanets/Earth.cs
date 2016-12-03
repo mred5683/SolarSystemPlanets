f6d28126e8808bd98e8597305b43c3b8a19b54a0
mred5683 <hansenrd5683@gmail.com>
2016-12-03 01:00:21 -0800

    add solarsystemplanets to repo

---
+++ SolarSystemPlanets/Earth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystemPlanets {
+  public class Earth {
+    public double AverageSurfaceTemp { get; set; }
+    public double AverageCoreTemp { get; set; }
+
+    public Earth(double averageSurfaceTemp, double averageCoreTemp) {
+      AverageSurfaceTemp = averageSurfaceTemp;
+      AverageCoreTemp = averageCoreTemp;
+    }
+  }
+}
---
+++ SolarSystemPlanets/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SolarSystemPlanets {
+  public class Program {
+    public static void Main() {
+      var earth = new Earth(67, 10000);
+      Console.WriteLine(Convert.ToString(earth).Single());
+      Console.ReadLine();
+    }
+  }
+}
